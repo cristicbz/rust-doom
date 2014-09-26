419abd937a7c3f95e851907b9edd5f303f506e7d
Cristian Cobzarenco <cristi.cobzarenco@gmail.com>
2014-09-26 22:34:10 +0100

    Added animated flats rendering.

--- src/shaders/flat.vertex.glsl
+++ src/shaders/flat.vertex.glsl
@@ -1,19 +1,35 @@
 #version 330 core
 
 uniform mat4 u_transform;
+uniform float u_time;
+uniform vec2 u_atlas_size;
 
 layout(location = 0) in vec3 a_pos;
 layout(location = 1) in vec2 a_offset;
 layout(location = 2) in float a_brightness;
+layout(location = 3) in int a_num_frames;
+layout(location = 4) in int a_frame_offset;
 
 flat out vec2 v_offset;
 flat out float v_brightness;
 out float v_dist;
 out vec3 v_pos;
 
+const float TICK_RATE = 8.0 / 35.0;
+const float TILE_SIZE = 64.0;
+
 void main() {
   v_pos = a_pos;
-  v_offset = a_offset;
+  if (a_num_frames == 1) {
+      v_offset = a_offset;
+  } else {
+      float frame_index = mod(floor(u_time / TICK_RATE) + a_frame_offset,
+                              a_num_frames);
+      float offset_x = a_offset.x + frame_index * TILE_SIZE;
+      float offset_y = a_offset.y +
+                       floor(offset_x / u_atlas_size.x) * TILE_SIZE;
+      v_offset = vec2(offset_x, offset_y);
+  }
   v_brightness = a_brightness;
   vec4 projected_pos = u_transform * vec4(a_pos, 1);
   v_dist = projected_pos.w;
