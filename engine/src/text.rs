88f3127a084e46a0ce721b70af498298f29511fa
Bradford Hovinen <hovinen@gmail.com>
2024-01-28 17:11:13 +0100

    Remove depth stencil from text pipeline

--- engine/src/text.rs
+++ engine/src/text.rs
@@ -309,13 +309,7 @@ impl<'context> System<'context> for TextRenderer {
                     polygon_mode: wgpu::PolygonMode::Fill,
                     conservative: false,
                 },
-                depth_stencil: Some(wgpu::DepthStencilState {
-                    format: wgpu::TextureFormat::Depth32Float,
-                    depth_write_enabled: true,
-                    depth_compare: wgpu::CompareFunction::Less,
-                    stencil: wgpu::StencilState::default(),
-                    bias: wgpu::DepthBiasState::default(),
-                }),
+                depth_stencil: None,
                 multisample: wgpu::MultisampleState {
                     count: 1,
                     mask: !0,
